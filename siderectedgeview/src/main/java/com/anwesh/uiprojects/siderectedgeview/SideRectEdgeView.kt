a63cc3306d1aebcbfda34b9907e49b91a51aecc4
Anwesh43 <anweshthecool0@gmail.com>
2019-05-05 12:39:29 +0530

    adding SRENode class

--- siderectedgeview/src/main/java/com/anwesh/uiprojects/siderectedgeview/SideRectEdgeView.kt
+++ siderectedgeview/src/main/java/com/anwesh/uiprojects/siderectedgeview/SideRectEdgeView.kt
@@ -132,4 +132,48 @@ class SideRectEdgeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    class SRENode(var i : Int, val state : State = State()) {
+
+        private var next : SRENode? = null
+        private var prev : SRENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SRENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRENode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRENode {
+            var curr : SRENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
