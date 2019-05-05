77cf52ae47b27883cf31553f05bc5bdebf02e7fa
Anwesh43 <anweshthecool0@gmail.com>
2019-05-05 12:51:15 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsiderectedgeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsiderectedgeview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsiderectedgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.siderectedgeview.SideRectEdgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SideRectEdgeView.create(this)
     }
 }
