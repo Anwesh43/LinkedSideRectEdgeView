399d60b83d7f8fb8682c848ab84866993c3fcdd9
Anwesh43 <anweshthecool0@gmail.com>
2019-05-05 12:11:24 +0530

    adding libraries and constants

--- siderectedgeview/src/main/java/com/anwesh/uiprojects/siderectedgeview/SideRectEdgeView.kt
+++ siderectedgeview/src/main/java/com/anwesh/uiprojects/siderectedgeview/SideRectEdgeView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.siderectedgeview
 /**
  * Created by anweshmishra on 05/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
